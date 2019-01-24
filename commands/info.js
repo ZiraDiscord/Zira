5625a18b04dd0b875cc20d2375c1ae14f42514b5
Hazed SPaCEx <hazed@zira.pw>
2019-01-23 18:23:29 -0800

    Info: Added guild count

--- commands/info.js
+++ commands/info.js
@@ -3,6 +3,11 @@
 // eslint-disable-next-line no-unused-vars
 exports.Run = async function Run(caller, command, guild, lang) {
   const changelog = await caller.utils.getChangelog();
+  const stats = await caller.ipc.getStats(command.msg.id);
+  let guilds = 0;
+  Object.keys(stats.data).forEach((key) => {
+    guilds += stats.data[key].guilds.length;
+  });
   const embed = {
     color: caller.color.blue,
     fields: [
@@ -26,14 +31,21 @@ exports.Run = async function Run(caller, command, guild, lang) {
         value: 'Hazed SPaCEx#2574\nJakeyPrime#0001',
         inline: true,
       },
+      {
+        name: lang.commands.info.fields[4],
+        value: guilds,
+        inline: true,
+      },
     ],
     footer: {
-      text: `Cluster: ${caller.id} Shard: ${(command.guild) ? command.guild.shard.id : 0} | Uptime: ${caller.utils.getTime(caller.bot.startTime)}`,
+      text: `Cluster: ${caller.id} Shard: ${
+        command.guild ? command.guild.shard.id : 0
+      } | Uptime: ${caller.utils.getTime(caller.bot.startTime)}`,
     },
   };
   if (changelog.changes) {
     embed.fields.push({
-      name: lang.commands.info.fields[4],
+      name: lang.commands.info.fields[5],
       value: changelog.changelog,
     });
   }
