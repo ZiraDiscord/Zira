a62c76d7bab1a27c0881f9602bd42b49c55efc2d
Hazed SPaCEx <hazed@zira.pw>
2019-01-23 18:10:43 -0800

    Added vote command

---
+++ commands/vote.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+exports.Run = async function Run(caller, command, guild, lang) {
+  caller.utils.createMessage(command.msg.channel.id, {
+    embed: {
+      color: caller.color.blue,
+      description: lang.commands.vote.description.replace('$link', `https://discordbots.org/bot/275813801792634880/vote${command.guild ? `?guild=${command.guild.id}` : ''}`),
+    },
+  });
+};
+
+exports.Settings = {
+  command: 'vote',
+  category: 3,
+  show: true,
+  permissions: [],
+  dm: false,
+};
