5a68e10b058bd6946df9090ad2f560fc3c8b74a0
Hazed SPaCEx <hazed@zira.pw>
2019-01-30 17:17:20 -0800

    Update Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -574,6 +574,14 @@ class Utils {
           },
     };
   }
+
+  arraysEqual(array1, array2) {
+    if (array1.length !== array2.length) return false;
+    for (let i = array1.length; i--; ) {
+      if (array1[i] !== array2[i]) return false;
+    }
+    return true;
+  }
 }
 
 module.exports = Utils;
