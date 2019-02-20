c344d336d39cac61429ec5c9185e646df5459e86
HazedSPaCEx <HazedSPaCEx@users.noreply.github.com>
2019-02-20 14:45:06 -0800

    Added newline.

--- commands/leave.js
+++ commands/leave.js
@@ -66,7 +66,7 @@ exports.Run = async function Run(caller, command, guild, lang) {
     case 'list': {
       let messages = '';
       guild.leaveMessage.forEach((message, index) => {
-        messages += `${index}. ${message}`;
+        messages += `${index}. ${message}\n`;
       });
       if (!messages.length) messages = lang.commands.leave.noMessages;
       caller.utils.createMessage(command.msg.channel.id, {
