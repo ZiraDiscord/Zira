8ce5500747135550347c768a5adb535c2f47909d
Hazed SPaCEx <hazed@zira.pw>
2019-02-24 18:13:31 -0800

    Fixed replace of undefined

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,50 +5,49 @@ exports.Run = async function Run(caller, guild, member, GuildDB) {
   if (Guild.joinChannel && Guild.joinMessage && Guild.joinMessage.length) {
     if (!member.user.bot) {
       let msg = Guild.joinMessage[caller.utils.randomNumber(0, Guild.joinMessage.length - 1)];
-      msg = msg.replace(/\$user/g, member.username);
-      msg = msg.replace(/\$mention/g, `<@${member.id}>`);
-      msg = msg.replace(/\$guild/g, guild.name);
-      msg = msg.replace(/\$membercount/g, guild.memberCount);
-      caller.bot.createMessage(Guild.joinChannel, msg).catch((e) => {
-        caller.logger.warn(
-          `[guildMemberAdd] ${e.code} ${e.message.replace(
-            /\n\s/g,
-            '',
-          )}`,
-        );
-        if (e.code === 50013 || e.code === 50001) {
-          Guild.joinChannel = '';
-          caller.utils.updateGuild(Guild);
-        }
-      });
+      if (msg) {
+        msg = msg.replace(/\$user/g, member.username);
+        msg = msg.replace(/\$mention/g, `<@${member.id}>`);
+        msg = msg.replace(/\$guild/g, guild.name);
+        msg = msg.replace(/\$membercount/g, guild.memberCount);
+        caller.bot.createMessage(Guild.joinChannel, msg).catch((e) => {
+          caller.logger.warn(`[guildMemberAdd] ${e.code} ${e.message.replace(/\n\s/g, '')}`);
+          if (e.code === 50013 || e.code === 50001) {
+            Guild.joinChannel = '';
+            caller.utils.updateGuild(Guild);
+          }
+        });
+      }
     }
   }
   switch (member.bot) {
     case false:
       if (!Guild.user[0]) return;
-      guild.editMember(member.id, {
-        roles: Guild.user,
-      }, 'Autorole on join').catch((e) => {
-        caller.logger.warn(
-          `[guildMemberAdd] ${e.code} ${e.message.replace(
-            /\n\s/g,
-            '',
-          )}`,
-        );
-      });
+      guild
+        .editMember(
+          member.id,
+          {
+            roles: Guild.user,
+          },
+          'Autorole on join',
+        )
+        .catch((e) => {
+          caller.logger.warn(`[guildMemberAdd] ${e.code} ${e.message.replace(/\n\s/g, '')}`);
+        });
       break;
     case true:
       if (!Guild.bot[0]) return;
-      guild.editMember(member.id, {
-        roles: Guild.bot,
-      }, 'Autorole on join').catch((e) => {
-        caller.logger.warn(
-          `[guildMemberAdd] ${e.code} ${e.message.replace(
-            /\n\s/g,
-            '',
-          )}`,
-        );
-      });
+      guild
+        .editMember(
+          member.id,
+          {
+            roles: Guild.bot,
+          },
+          'Autorole on join',
+        )
+        .catch((e) => {
+          caller.logger.warn(`[guildMemberAdd] ${e.code} ${e.message.replace(/\n\s/g, '')}`);
+        });
       break;
     default:
   }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -5,19 +5,19 @@ exports.Run = async function Run(caller, guild, member, GuildDB) {
   if (Guild.leaveChannel && Guild.leaveMessage) {
     if (!member.user.bot) {
       let msg = Guild.leaveMessage[caller.utils.randomNumber(0, Guild.leaveMessage.length - 1)];
-      msg = msg.replace(/\$user/g, member.user.username);
-      msg = msg.replace(/\$mention/g, `<@${member.id}>`);
-      msg = msg.replace(/\$guild/g, guild.name);
-      msg = msg.replace(/\$membercount/g, guild.memberCount);
-      caller.bot.createMessage(Guild.leaveChannel, msg).catch((e) => {
-        caller.logger.warn(
-          `[guildMemberRemove] ${e.code} ${e.message.replace(/\n\s/g, '')}`,
-        );
-        if (e.code === 50013 || e.code === 50001) {
-          Guild.leaveChannel = '';
-          caller.utils.updateGuild(Guild);
-        }
-      });
+      if (msg) {
+        msg = msg.replace(/\$user/g, member.user.username);
+        msg = msg.replace(/\$mention/g, `<@${member.id}>`);
+        msg = msg.replace(/\$guild/g, guild.name);
+        msg = msg.replace(/\$membercount/g, guild.memberCount);
+        caller.bot.createMessage(Guild.leaveChannel, msg).catch((e) => {
+          caller.logger.warn(`[guildMemberRemove] ${e.code} ${e.message.replace(/\n\s/g, '')}`);
+          if (e.code === 50013 || e.code === 50001) {
+            Guild.leaveChannel = '';
+            caller.utils.updateGuild(Guild);
+          }
+        });
+      }
     }
   }
   const once = await caller.db.get('once');
